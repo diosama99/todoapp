b2e390c1c915cb3325fb26ee5012bcaaf8ea0991
Elena Daurtseva <diosama99@gmail.com>
2024-01-19 23:25:18 +0700

    add print

--- GUI.py
+++ GUI.py
@@ -2,11 +2,14 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText()
+input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box], [add_button]])
-window.read()
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box], [add_button]],
+                   font=('Helvetica', 20))
+event = window.read()
+print(event)
 window.close()
 
 
